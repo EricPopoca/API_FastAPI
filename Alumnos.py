a28ca99a35a04276a73a788385089cb69db10ea4
Eric Popoca <eric.mlpopoca@gmail.com>
2024-12-12 16:49:54 -0600

    Referente al guardado de datos

--- Alumnos.py
+++ Alumnos.py
@@ -18,3 +18,25 @@ class Alumno(BaseModel):
 app = FastAPI()
 
 #Para la persistencia de datos se crea el archivo JSON
+DATA_FILE = "alumnos.json"
+
+#Base de datos local
+alumnos_db : List[Alumno] = []
+
+#Carga y guardado de datos del JSON a la memoria local
+#Cargar datos
+def cargar_datos():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as file:
+            datos = json.load(file)
+            return [Alumno(**alumno) for alumno in datos]
+    return []
+
+#Guardar datos
+def guardar_datos():
+    with open(DATA_FILE, "w") as file:
+        json.dump([alumno.dict() for alumno in alumnos_db], file, indent=4)
+
+#Inicializar la base de datos
+alumnos_db = cargar_datos()
+
